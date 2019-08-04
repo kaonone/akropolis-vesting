45a5406d7b1ea5ce610cc2fe73b0230f6e5c3356
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2019-08-04 09:52:01 +0300

    fix all_16102019

--- migrations/3_all_16092019.js
+++ migrations/3_all_16092019.js
@@ -4,7 +4,7 @@ var AkropolisTimeLock = artifacts.require("./AkropolisTimeLock.sol");
 module.exports = function(deployer, network, accounts) {
   let owner = accounts[0];
   
-  let token = "0x8ab7404063ec4dbcfd4598215992dc3f8ec853d7";
+  let token = "0x8ab7404063ec4dbcfd4598215992dc3f8ec853d7"; //AKRO
   let releaseDate = 1568592060;
   
   //console.log('owner of storage contracts: ' + owner)
